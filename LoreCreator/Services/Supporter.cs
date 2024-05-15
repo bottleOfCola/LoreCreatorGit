224fc190f41f6c5b453cbe9334229027528e8057
koshelock <kosheii@yandex.ru>
2024-05-15 11:06:51 +0300

    Добавил локализацию для Profile, GetRole и Полномочий. Исправил переход к GetRole

--- LoreCreator/Services/Supporter.cs
+++ LoreCreator/Services/Supporter.cs
@@ -9,17 +9,22 @@ public static class Supporter
     public static string GetNameOfPermission(PermissionTypes permission)
         => permission switch
         {
-            PermissionTypes.Unknown => "Ошибка",
-            PermissionTypes.AddMessage => "Отправка сообщений",
-            PermissionTypes.RemoveMessage => "Удаление сообщений",
-            PermissionTypes.AddElement => "Добавление элемента",
-            PermissionTypes.RemoveElement => "Удаление элемента",
-            PermissionTypes.AddTag => "Добавление тэга",
-            PermissionTypes.RemoveTag => "Удаление тэга",
-            PermissionTypes.AddConnectionType => "Добавление типа связи",
-            PermissionTypes.RemoveConnectionType => "Удаление типа связи",
-            PermissionTypes.AddConnection => "Добавление связи",
-            PermissionTypes.RemoveConnection => "Удаление связи"
+            PermissionTypes.Unknown => LCLPermissionTypes.Unknown,
+            PermissionTypes.AddMessage => LCLPermissionTypes.AddMessage,
+            PermissionTypes.RemoveMessage => LCLPermissionTypes.RemoveMessage,
+            PermissionTypes.AddElement => LCLPermissionTypes.AddElement,
+            PermissionTypes.RemoveElement => LCLPermissionTypes.RemoveElement,
+            PermissionTypes.AddConnection => LCLPermissionTypes.AddConnection,
+            PermissionTypes.RemoveConnection => LCLPermissionTypes.RemoveConnection,
+            PermissionTypes.AddConnectionType => LCLPermissionTypes.AddConnectionType,
+            PermissionTypes.RemoveConnectionType => LCLPermissionTypes.RemoveConnectionType,
+            PermissionTypes.AddTag => LCLPermissionTypes.AddTag,
+            PermissionTypes.RemoveTag => LCLPermissionTypes.RemoveTag,
+            PermissionTypes.RoleGiving => LCLPermissionTypes.RoleGiving,
+            PermissionTypes.RoleBacking => LCLPermissionTypes.RoleBacking,
+            PermissionTypes.AddRole => LCLPermissionTypes.AddRole,
+            PermissionTypes.RemoveRole => LCLPermissionTypes.RemoveRole,
+            _ => LCLPermissionTypes.Error,
         };
     public static List<PermissionTypes> GetPermissionsFromString(IEnumerable<Claim> claims)
         => claims.FirstOrDefault(x => x.Type == "permissions")?.Value.Split(' ').ToList().ConvertAll(x => (PermissionTypes)int.Parse(x)) ?? new List<PermissionTypes>();
